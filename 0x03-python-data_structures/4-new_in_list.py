bd5aaac373aaeb1afc67ea5965b36fe7c9917fd3
Hosam Elsheikh <hosam.selsheikh@gmail.com>
2024-01-06 18:11:17 -0800

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"replaces an element in a list at idx without modifying the original list"
 
 def new_in_list(my_list, idx, element):
     my_list_2 = my_list[:]
+
     if idx < 0 or idx >= len(my_list):
         return(my_list_2)
     my_list_2[idx] = element
