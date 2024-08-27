06da3418e37699269942830107db5e9d474dffe2
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-08-27 23:04:44 +0300

    xoxo

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    "replaces an element in a list at idx without modifying the original list"
-
-    my_list_copy = my_list[:]
-    "if idx is negative or out of range, return a copy of the original list"
-    if idx < 0 or idx >= len(my_list):
-        return (my_list_copy)
-    my_list_copy[idx] = element
-    return (my_list_copy)
+    cpd_list = my_list[:]
+    if (idx < 0 or idx >= len(my_list)):
+        return (cpd_list)
+    cpd_list[idx] = element
+    return (cpd_list)
