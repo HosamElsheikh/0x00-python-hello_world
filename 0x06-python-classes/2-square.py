76e60dc613e2d68bd6d30bb3ce44deb0c368fbcf
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-06 15:22:41 +0300

    Let's see

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-"""square class main"""
+"""This class is to generate a square"""
 
 
-class Square():
-    """square class"""
+class Square:
+    """This class is to create a square"""
     def __init__(self, size=0):
-        """ Instance of class Square
-    Arguments:
-        @size: size of side of square"""
+        """Creates an instance of the class
 
+        Arguments:
+            @size: The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: Only integeres are accepted in size
+            ValueError: size can't be a negative value
+        """
+        
         self.__size = size
+        
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
