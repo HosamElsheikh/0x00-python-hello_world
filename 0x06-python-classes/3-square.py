4042be227eb15b1488d6360c2f3d033ce597eef1
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-06 15:36:24 +0300

    Is this really the mistake?

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""square class main"""
+
+"""Define a class Square."""
 
 
-class Square():
-    """square class"""
+class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """ Instance of class Square
-    Arguments:
-        @size: size of side of square"""
+        """Initialize a new Square.
 
-        self.__size = size
-        if type(size) != int:
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
+        self.__size = size
+        
     def area(self):
-        """ area of square
-        Return:
-                area of square."""
+        """Returns the area of the square at hand"""
         return self.__size ** 2
