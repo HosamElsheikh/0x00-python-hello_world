c1affcd4ef8f87958af99cd8c9f050465916f7e4
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-06 15:52:44 +0300

    ew3aaa el squarezzz

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python3
-"""square class main"""
+
+"""Define a class Square."""
 
 
-class Square():
-    """square class"""
+class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """ Instance of class Square
-    Arguments:
-        @size: size of side of square"""
+        """Initialize a new Square.
 
-        self.__size = size
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        Args:
+            size (int): The size of the new square.
+        """
+
+        self.size = size
 
     def area(self):
-        """ area of square
-        Return:
-                area of square."""
-        return self.__size ** 2
+        """Returns the area of the square at hand"""
+        return self.__size * self.__size
 
     @property
     def size(self):
-        """ getter of size
-    Return:
-            value of size"""
+        """This function returns the value of the size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ setter of the size
-    Arguments:
-        value: value of size"""
-        self.__size = value
-        if type(value) != int:
+        """This function sets a size for the given square
+
+        Args:
+            value (int): The length of any of the square sides
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
 
     def my_print(self):
-        """ square made using the character #
-            or a blank line if size == 0"""
-        if (self.size == 0):
+        """This function results in printing the square"""
+        if self.__size == 0:
             print("")
-        for i in range(self.size):
-            print("#" * self.size)
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
