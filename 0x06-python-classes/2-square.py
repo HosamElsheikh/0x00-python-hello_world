5df8cc7801977a3298638e4a25463671109900f9
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-06 15:26:26 +0300

    Is it the raises?

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,15 +8,9 @@ class Square:
         """Creates an instance of the class
 
         Arguments:
-            @size: The size of the square. Defaults to 0.
-
-        Raises:
-            TypeError: Only integeres are accepted in size
-            ValueError: size can't be a negative value
-        """
+            @size: The size of the square. Defaults to 0."""
         
         self.__size = size
-        
         if (type(size) != int):
             raise TypeError("size must be an integer")
         elif (size < 0):
