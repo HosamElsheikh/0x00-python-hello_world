e70d6ed01784a0126b471b39b4d8d69c767da387
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-06 15:30:35 +0300

    DID I JUST MISS THE INDENTATION ??!?!?!?!?

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,13 +4,14 @@
 
 class Square():
     """square class"""
+
     def __init__(self, size=0):
         """ Instance of class Square
     Args:
-        @size (int): size of side of square"""
-
-        if type(size) != int:
+        @size (int): size of side of square
+        """
+        if not isinstance(int, size):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-            self.__size = size
+        self.__size = size
