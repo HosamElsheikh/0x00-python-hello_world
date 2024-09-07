e9194e2561ddafbf5018ba6e2854704b5d6275da
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-07 23:48:29 +0300

    FUCKING HELL

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
-"""print_square"""
+"""This prints a square"""
 
 
 def print_square(size):
-        """ print_square
-        Arguments:
-        @size: size of square
-                Return: nothing"""
-        if isinstance(size, float) and size < 0:
-                raise TypeError("size must be an integer")
-        elif not isinstance(size, int):
-                raise TypeError("size must be an integer")
-        elif size < 0:
-                raise TypeError("size must be >= 0")
-        elif size == 0:
-                return
-        for row in range(1, size + 1):
-                print("#" * size)
+    """This is a function to print #s to form a square
+
+    Args:
+        size (int): The size of the square
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+        return
+    if size < 0:
+        raise ValueError("size must be >= 0")
+        return
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+        return
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
