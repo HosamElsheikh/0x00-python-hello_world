66ff5dd0e833aeb9382c3af0f706ca0feb16c844
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-07 18:21:13 +0300

    Added a few validation checks

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""say_my_name """
+"""Say my name. Heisenberg"""
 
 
 def say_my_name(first_name, last_name=""):
-        """ say_my_name
-        Arguments:
-        @first_name: first_name
-        @last_name: last_name (optional)"""
-        if not isinstance(first_name, str):
-                raise TypeError("first_name must be a string")
-        elif not isinstance(last_name, str):
-                raise TypeError("last_name must be a string")
-        else:
-                print("My name is {} {}".format(first_name, last_name))
+    """This function says the name of the user
+    Args:
+        @first_name: The first name
+        @last_name: The last name"""
+    if first_name is None:
+        raise TypeError("first_name must be a string")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+        return
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+        return
+    print("My name is {} {}".format(first_name, last_name))
