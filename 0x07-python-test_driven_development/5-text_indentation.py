2514fa70c8b53e1440c2e8a9cc4f2a559f986161
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-08 00:37:34 +0300

    I did ittttt

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-""" text_indentation"""
+"""This function prints two lines after the period
+comma, or question mark
+"""
 
 
 def text_indentation(text):
-        """ text_indentation
-        Arguments:
-        @text: text to be changed"""
-        if type(text) is not str:
-                raise TypeError("text must be a string")
-        text = text.strip()
-        if len(text) == 0:
-                return
-        temp = text[0]
-        delims = ['.', ':', '?']
-        for char in text:
-                if char == ' ' and temp in delims:
-                        continue
-                temp = char
-                print(char, end='')
-                if char in delims:
-                        print()
-                        print()
+    """This function adds an extra line.
+    Args:
+        @text: The text
+    """
+    skip_space = False
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for i in text:
+        if skip_space is True:
+            skip_space = False
+            continue
+        elif i in ".?:":
+            print(i)
+            print("")
+            skip_space = True
+        else:
+            print(i, end="")
