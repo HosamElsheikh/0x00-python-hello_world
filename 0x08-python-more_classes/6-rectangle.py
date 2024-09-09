de6e8adbd6b393751764db7b3b16f85306153ec1
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-09 22:55:01 +0300

    yayayayyayaya

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,43 +1,29 @@
 #!/usr/bin/python3
-"""Rectangle class to represent a square"""
+"""This creates a python class"""
 
 
-class Rectangle():
-    """Rectangle Class"""
-    number_of_instances = 0
-
+class Rectangle:
+    """This is a rectangle class"""
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
+        """Initializes a new rectangle class.
+        Ars:
+            @width (int): The width of the triangle
+            @height(int): The height of the triangle
+        """
+        self.width = width
+        self.height = height
         type(self).number_of_instances += 1
-        self.__width = width
-        self.__height = height
 
     @property
     def width(self):
-        """ width of rectangle
-        Return:
-        width of rectangle."""
         return self.__width
 
     @property
     def height(self):
-        """ height of rectangle
-        Return:
-        height of rectangle."""
         return self.__height
 
     @width.setter
     def width(self, value):
-        """ setter of the width
-        Arguments:
-        value: value of width"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -46,9 +32,6 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
-        """ setter of the width
-        Arguments:
-        value: value of height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -56,38 +39,35 @@ class Rectangle():
         self.__height = value
 
     def area(self):
-        """ area of rectangle
-        Return:
-        area of rectangle."""
-        return self.__width * self.__height
+        """This one returns the area of the rectangle"""
+        return self.__height * self.__width
 
     def perimeter(self):
-        """ perimeter of rectangle
-        Return:
-        perimeter of rectangle."""
+        """This one returns the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
-        else:
-            return 2 * (self.__width + self.__height)
+        return (self.__height + self.__width) * 2
 
     def __str__(self):
-        """ rectangle made using the character #
-        """
-        string = ""
+        """This is used when printing an instance of the class"""
+        str = ""
         if self.__width == 0 or self.__height == 0:
-            return string
-        for row in range(self.__height):
-            if row < (self.__height - 1):
-                string += ("#" * self.__width) + "\n"
-            else:
-                string += ("#" * self.__width)
-        return string
+            return str
+        for i in range(self.__height):
+            for j in range(self.__width):
+                str += "#"
+            if i == self.__height - 1:
+                break
+            str += "\n"
+        return str
 
     def __repr__(self):
-        """ represent self for eval"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        """This is a special method for the code to create the string"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
-        """delete self and count"""
-        type(self).number_of_instances -= 1
+        """This is called to delete an instance of the class"""
         print("Bye rectangle...")
+        type(self).number_of_instances -= 1
+
+    number_of_instances = 0
