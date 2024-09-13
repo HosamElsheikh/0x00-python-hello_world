326d96e22159810153fa9add94eae44f16da0caa
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-13 17:47:27 +0300

    Why import all??

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,47 +1,16 @@
 #!/usr/bin/python3
-"""contains classes to represent different shapes"""
+"""This is a square module"""
 
 
-class BaseGeometry():
-    """BaseGeometry class"""
-    def area(self):
-        """raises an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates an int"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class to represent rectangles"""
-    def __init__(self, width, height):
-        """initializer"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """evaluates the area"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """formatted output"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
-    """Square class to represent squares"""
+    """This is the square class that we will impelement"""
     def __init__(self, size):
-        """initializer"""
         self.integer_validator("size", size)
-        super().__init__(size, size)
+        Rectangle.__init__(self, size, size)
         self.__size = size
 
     def __str__(self):
-        """formatted output"""
-        return ("[Square] {}/{}".format(self.__size, self.__size))
+        return "[Square] {}/{}".format(self.__size, self.__size)
