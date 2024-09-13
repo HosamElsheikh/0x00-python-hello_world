127355550d8d3993bf08bd7452af114fbe9549d5
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-13 17:04:46 +0300

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""inheritance from list"""
+"""Docstring for the class"""
 
 
 def inherits_from(obj, a_class):
-    """inherit"""
-    if issubclass(type(obj), (a_class)) and type(obj) is not a_class:
-        return True
-    return False
+    """This implements the functionality.
+    Args:
+        @obj: This is the object we are checking
+        @a_class: This checks the class
+        """
+    return issubclass(type(obj), a_class) and not type(obj) is a_class
