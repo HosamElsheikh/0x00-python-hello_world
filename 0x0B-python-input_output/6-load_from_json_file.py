8bb41d0f36fb00c2ca356aa0ecbdff15aca604ca
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-09-24 22:53:34 +0300

    Testing it with load instead of loads :/

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,4 +7,4 @@ import json
 def load_from_json_file(filename):
     """creates an Object from a “JSON file” """
     with open(filename, encoding='utf-8') as file:
-        return (json.loads(file.read()))
+        return (json.load(file))
