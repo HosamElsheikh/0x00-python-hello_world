5e4315d6344a995e26749fdced69bee7e2e7052a
HosamElsheikh <102907236+HosamElsheikh@users.noreply.github.com>
2024-12-18 19:09:11 +0200

    Why are we flooring??

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-function add (a, b) {
+function add(a, b) {
 	return a + b;
-  }
-  
-  console.log(add(Math.floor(Number(process.argv[2])), Math.floor(Number(process.argv[3]))));
+}
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
